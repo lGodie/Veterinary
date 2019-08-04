bad1d6502bf7d5a00b5a15071dbb7965f6ad469b
DIEGO ALEJANDRO ZAPATA VANEGAS <diegozapata176738@correo.itm.edu.co>
2019-08-03 22:05:50 -0500

    Bd Controller owner seed db

---
+++ Veterinary/Veterinary/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Veterinary.Data.Entities;
+
+namespace Veterinary.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Owner> Owners { get; set; }
+
+        public DbSet<PetType> PetTypes { get; set; }
+
+        public DbSet<Pet> Pets { get; set; }
+
+        public DbSet<ServiceType> ServiceTypes { get; set; }
+
+        public DbSet<History> Histories { get; set; }
+
+        public DbSet<Agenda> Agendas { get; set; }
+
+    }
+
+}
---
+++ Veterinary/Veterinary/Data/Entities/PetType.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Veterinary.Data.Entities
+{
+    public class PetType
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pet Type*")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Name { get; set; }
+    }
+    
+}
